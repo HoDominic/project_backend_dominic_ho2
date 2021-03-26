10591f4c5f0be63e81b1ce2d6233a6c8e74a509f
HoDominic <dominic.ho@student.howest.be>
2021-03-26 15:40:19 +0100

    added migration

--- BookAPI/DTO/AuthorDTO.cs
+++ BookAPI/DTO/AuthorDTO.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookAPI.DTO
 {
     public class AuthorDTO
     {
+
+        [Key]
         public Guid AuthorId { get; set; }
         public string Name { get; set; }
     }
